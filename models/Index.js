315e6bbc85fd0a677ebfe513c3c4bc8fdb88ea00
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-07-11 00:19:26 -0500

    index model

--- models/Index.js
+++ models/Index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Categor, {
+  foreignKey: 'categoryID',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'categoryID',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'productID',
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tagID'
+});
 
 module.exports = {
   Product,
