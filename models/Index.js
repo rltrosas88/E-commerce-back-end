9d0b51413501a9855effcfea53a9fb1912b7666f
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-07-11 00:21:41 -0500

    typo

--- models/Index.js
+++ models/Index.js
@@ -5,7 +5,7 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Categor, {
+Product.belongsTo(Category, {
   foreignKey: 'categoryID',
 });
 // Categories have many Products
